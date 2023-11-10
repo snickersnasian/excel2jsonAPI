abdcfb6c6059024ed131c84307a75ec3bac22a2a
Duy Ahn Nguen/Automation Part//SDSE <nguen.d@samsung.com>
2023-11-10 15:20:10 +0300

    common route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { router: xlsxRouter} = require("./routes/xlsx.routes");
+const { router: xlsxRouter } = require("./routes/xlsx.routes");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +9,14 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use('/api', xlsxRouter)
+
+app.get('*', (req, res) => {
+  res.status(200).json({
+    route: req.url,
+    api: 'Excel2JSON',
+    abailable: true,
+  });
+})
 
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
